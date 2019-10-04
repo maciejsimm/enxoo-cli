bc68b5188943dc0ef1bcacd0121324eb535e0b7a
adrianLukaszekEnxoo <adrian.lukaszek@enxoo.com>
2019-10-04 12:57:45 +0200

    removed unnecessary comments

--- src/commands/enxoo/cpq/prd/retrieve.ts
+++ src/commands/enxoo/cpq/prd/retrieve.ts
@@ -15,21 +15,6 @@ export default class Org extends SfdxCommand {
 
   public static description = messages.getMessage('commandDescription');
 
-  public static examples = [
-  `$ sfdx enxoo:cpq:prd:retrieve --u myOrg@example.com -p 'GEPL,IPLC,Colocation Service'
-  *** Begin exporting GEPL,IPLC products ***
-  --- exporting product definition 
-  --- exporting product attributes 
-  `,
-  `$ sfdx enxoo:cpq:prd:retrieve --u myOrg@example.com -p *ALL
-  *** Begin exporting all products ***
-  --- exporting product definition 
-  ...
-  `
-  ];
-
-  // public static args = [{name: 'file'}];
-
   protected static flagsConfig = {
     products: flags.array({char: 'p', required: true, description: messages.getMessage('productsFlagDescription')}),
     b2b: flags.boolean({char: 'b', required: false, description: messages.getMessage('b2bFlagDescription')}),
@@ -53,7 +38,7 @@ export default class Org extends SfdxCommand {
     let conn: Connection;
     conn = await getJsforceConnection(this.org.getConnection().getConnectionOptions());
     conn.bulk.pollInterval = 5000; // 5 sec
-    conn.bulk.pollTimeout = 300000; // 180 sec
+    conn.bulk.pollTimeout = 300000; // 300 sec
     const products = this.flags.products;
     const b2b = this.flags.b2b;
     const dir = this.flags.dir;
@@ -66,46 +51,6 @@ export default class Org extends SfdxCommand {
 
     this.ux.log('*** Finished ***');
     
-    // const query = 'Select Name, TrialExpirationDate from Organization';
-
-    // The type we are querying for
-    // interface Organization {
-    //   Name: string;
-    //   TrialExpirationDate: string;
-    // }
-
-    // Query the org
-    // const result = await conn.query<Organization>(query);
-
-    // Organization will always return one result, but this is an example of throwing an error
-    // The output and --json will automatically be handled for you.
-    // if (!result.records || result.records.length <= 0) {
-    //   throw new core.SfdxError(messages.getMessage('errorNoOrgResults', [this.org.getOrgId()]));
-    // }
-
-    // Organization always only returns one result
-    // const orgName = result.records[0].Name;
-    // const trialExpirationDate = result.records[0].TrialExpirationDate;
-
-    // let outputString = `Hello ${name}! This is org: ${orgName}`;
-    // if (trialExpirationDate) {
-    //   const date = new Date(trialExpirationDate).toDateString();
-    //   outputString = `${outputString} and I will be around until ${date}!`;
-    // }
-    // this.ux.log(outputString);
-
-    // this.hubOrg is NOT guaranteed because supportsHubOrgUsername=true, as opposed to requiresHubOrgUsername.
-    // if (this.hubOrg) {
-    //   const hubOrgId = this.hubOrg.getOrgId();
-    //   this.ux.log(`My hub org id is: ${hubOrgId}`);
-    // }
-
-    // if (this.flags.force && this.args.file) {
-    //   this.ux.log(`You input --force and a file: ${this.args.file}`);
-    // }
-
-    // Return an object to be displayed with --json
-    // return { orgId: this.org.getOrgId(), outputString };
     return null;
   }
 }
