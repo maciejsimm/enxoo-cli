74d74a782c41e140b62ceca2f5d1c515fe65c3af
JuliaSmutek <julia.smutek@enxoo.com>
2021-06-24 14:50:54 +0200

    ECPQ-5370 - add condition when there is no retrieved settings

--- src/lib/settings/SettingsImport.ts
+++ src/lib/settings/SettingsImport.ts
@@ -23,8 +23,10 @@ export class SettingsImport {
         const settingsSelector = new SettingsSelector(querySettings);
         const settingsTargetIds = await settingsSelector.getAllSettingIds(this.connection);
 
-        await Upsert.deleteData(this.connection, settingsTargetIds, 'enxCPQ__CPQ_Settings__c');
-        await Upsert.insertData(this.connection, settings, 'enxCPQ__CPQ_Settings__c');
+        if(settings.length){
+          await Upsert.deleteData(this.connection, settingsTargetIds, 'enxCPQ__CPQ_Settings__c');
+          await Upsert.insertData(this.connection, settings, 'enxCPQ__CPQ_Settings__c');
+        }
 
     }
 
